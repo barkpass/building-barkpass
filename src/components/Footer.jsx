0a1215cadbccb1bffc7ca260293ea187cf136b57
Josh Larson <jplhomer@gmail.com>
2021-11-12 20:30:00 -0600

    Use automatic year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -185,7 +185,7 @@ export default function Footer() {
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 xl:text-center">
-            © 2021 Barkpass, LLC. All rights reserved.
+            © {new Date().getFullYear()} Barkpass, LLC. All rights reserved.
           </p>
         </div>
       </div>
